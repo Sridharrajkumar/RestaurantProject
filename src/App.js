f7c7c3f011da9b2246d3826b061cfe00afd17550
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-06-07 23:12:29 +0530

    AddingPortal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Fragment} from "react";
 import Header from "./component/LayOut/Header";
+import Cart from "./component/Cart/Cart";
 
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />  
       
     </Fragment>
